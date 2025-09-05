dd0d85f28f95b909f1e1dc79acedb7459c064337
Carlos Fernando Riaño Bedoya <carlosestecnologia@gmail.com>
2025-09-05 19:11:46 +0000

    Anexo seccion de noticias

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Prueba from './secciones/prueba'
+import Riaño from './secciones/Riaño'
 import './App.css'
 
 function App() {
   return (
     <>
-      <Prueba />
+
+      <Riaño />
     </>
   )
 }
